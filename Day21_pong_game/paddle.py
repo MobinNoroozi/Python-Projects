a02e217aca1d6abfc656f1d5439e93c8cc410b6f
MobinNoroozi <144638986+MobinNoroozi@users.noreply.github.com>
2026-07-02 02:21:06 -0400

    Add comments for paddle properties and movement methods
    Added comments to clarify paddle properties and movement.

--- Day21_pong_game/paddle.py
+++ Day21_pong_game/paddle.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 
 class Paddle(Turtle):
     def __init__(self, xcord):
+        #Setting the properties of the paddle
         super().__init__()
         self.shape("square")
         self.color("white")
@@ -13,6 +14,7 @@ class Paddle(Turtle):
 
 
     def go_up(self):
+        #Moves by 20 pixels
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
