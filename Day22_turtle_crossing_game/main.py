0e4500381579659c623663ed76423445065d98ed
MobinNoroozi <144638986+MobinNoroozi@users.noreply.github.com>
2026-07-02 02:36:06 -0400

    Initialize game screen and player controls

--- Day22_turtle_crossing_game/main.py
+++ Day22_turtle_crossing_game/main.py
@@ -4,22 +4,25 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-
+#Create screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+#Create the objects and can only go up
 player = Player()
 carmanager = CarManager()
 scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(player.move, "Up")
 
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.1) #Every 0.1 second the screen updates
     screen.update()
 
+    #And that create and move cars
     carmanager.create_car()
     carmanager.car_move()
 
